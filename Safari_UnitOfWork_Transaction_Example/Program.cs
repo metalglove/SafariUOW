9af13dc1065466cf3bf72e80f1ba78b71c3ed76d
Mario Lucassen <mario.lucassen@hotmail.com>
2018-12-01 16:37:27 +0100

    Add project files.

---
+++ Safari_UnitOfWork_Transaction_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Safari_UnitOfWork_Transaction_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
