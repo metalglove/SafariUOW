041616b655b74d908f66355c39e8a417b552c9e8
Mario Lucassen <mario.lucassen@hotmail.com>
2018-12-05 14:03:42 +0100

    null propagation

--- Safari_UnitOfWork_Transaction_Example/Implementations/UnitOfWork.cs
+++ Safari_UnitOfWork_Transaction_Example/Implementations/UnitOfWork.cs
@@ -5,7 +5,7 @@ namespace Safari_UnitOfWork_Transaction_Example.Implementations
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private readonly IFactory<IUserRepository, SqlConnection> _userRepositoryfactory;
+        private readonly IFactory<IUserRepository, SqlConnection> _userRepositoryFactory;
         private readonly IFactory<SqlConnection> _connectionFactory;
 
         #region Repositories
@@ -13,36 +13,33 @@ namespace Safari_UnitOfWork_Transaction_Example.Implementations
         public IUserRepository UserRepository
         {
             get => _userRepository ?? 
-                (_userRepository = _userRepositoryfactory.Create(_connectionFactory.Create()));
+                (_userRepository = _userRepositoryFactory.Create(_connectionFactory.Create()));
         }
         #endregion Repositories
 
         public UnitOfWork(
-            IFactory<IUserRepository, SqlConnection> userRepositoryfactory,
+            IFactory<IUserRepository, SqlConnection> userRepositoryFactory,
             IFactory<SqlConnection> connectionFactory)
         {
-            _userRepositoryfactory = userRepositoryfactory;
+            _userRepositoryFactory = userRepositoryFactory;
             _connectionFactory = connectionFactory;
         }
 
         public bool Commit()
         {
-            if(_userRepository != null)
-                _userRepository.Commit();
+            _userRepository?.Commit();
 
             return true;
         }
         public bool Rollback()
         {
-            if (_userRepository != null)
-                _userRepository.Rollback();
+            _userRepository?.Rollback();
 
             return true;
         }
         public void Dispose()
         {
-            if (_userRepository != null)
-                _userRepository.Dispose();
+            _userRepository?.Dispose();
         }
     }
 }
